ecdb31919fe219efa34aeaeb57582d4e89d11867
Forest Eckhardt <feckhardt@pivotal.io>
2020-04-13 16:46:32 -0400

    stub out git.sh
    Co-authored-by: Josh Zarrabi <jzarrabi@pivotal.io>

--- scripts/.util/git.sh
+++ scripts/.util/git.sh
@@ -7,15 +7,16 @@ set -o pipefail
 source "$(dirname "${BASH_SOURCE[0]}")/print.sh"
 
 function util::git::token::fetch() {
-    if [[ -z "${GIT_TOKEN:-""}" ]]; then
-        util::print::title "Fetching GIT_TOKEN"
-
-        GIT_TOKEN="$(
-            lpass show Shared-CF\ Buildpacks/concourse-private.yml \
-                | grep buildpacks-github-token \
-                | cut -d ' ' -f 2
-        )"
-    fi
-
-    printf "%s" "${GIT_TOKEN}"
+    echo "hello"
+    # if [[ -z "${GIT_TOKEN:-""}" ]]; then
+    #     util::print::title "Fetching GIT_TOKEN"
+    #
+    #     GIT_TOKEN="$(
+    #         lpass show Shared-CF\ Buildpacks/concourse-private.yml \
+    #             | grep buildpacks-github-token \
+    #             | cut -d ' ' -f 2
+    #     )"
+    # fi
+    #
+    # printf "%s" "${GIT_TOKEN}"
 }
